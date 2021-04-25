a6efe3893b92bcebeab94a3e5f0d4ad1cc7f7253
Khanh Vuong <khanhvuong987@gmail.com>
2021-04-25 18:08:23 +0700

    'interface'

--- app/src/main/java/com/demo/carpooling/MainActivity.java
+++ app/src/main/java/com/demo/carpooling/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.cuberto.liquid_swipe.LiquidPager;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
